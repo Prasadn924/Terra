85128f86d0fbbea89c441d8a78a404d7307cce4d
Prasadn924 <74699858+Prasadn924@users.noreply.github.com>
2021-02-25 11:50:08 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "us-east-1"
-  access_key = "AKIA5PV5S5YNILK5OJXV"
-  secret_key = "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"
-}
-resource "aws_instance" "Myfirstinstance" {
-  ami = "ami-0915bcb5fa77e4892"
-  instance_type = "t2.micro"
-}  
